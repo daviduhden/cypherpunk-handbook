3e44f512bdd326ceed969f7bfe2a9967af8a186e
David Uhden Collado <david@uhden.dev>
2026-01-03 22:48:49 +0100

    Format scripts

--- scripts/rebuild-feeds.pl
+++ scripts/rebuild-feeds.pl
@@ -230,6 +230,6 @@ s{<lastBuildDate>[^<]*</lastBuildDate>}{<lastBuildDate>$now</lastBuildDate>}m;
     write_utf8( $feed, $new );
     logi("Wrote $feed");
 }
-else { die_tool "Could not find </channel> in template\n" }   
+else { die_tool "Could not find </channel> in template\n" }
 
 exit 0;
