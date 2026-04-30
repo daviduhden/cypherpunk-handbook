ac1730d1ccbf467b3d97f402cae8b5d70a361f84
David Uhden Collado <david@uhden.dev>
2026-04-30 21:30:22 +0200

    Various fixes and improvements

--- scripts/rebuild-feeds.pl
+++ scripts/rebuild-feeds.pl
@@ -1,256 +1,228 @@
 #!/usr/bin/perl
 
-# Copyright (c) 2025-2026 David Uhden Collado
-#
-# Permission to use, copy, modify, and distribute this software for any
-# purpose with or without fee is hereby granted, provided that the above
-# copyright notice and this permission notice appear in all copies.
-#
-# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
-# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-#
-# This script rebuilds feeds/articles.xml from data/articles.json
-# - Reads data/articles.json
-# - Ensures articles.json is written with keys sorted alphabetically
-# - Extracts first <p> from article files when available
-# - Writes feeds with UTF-8 encoding and RFC2822 pubDates
-# -------------------------
-# Usage: rebuild-feeds.pl
-# No arguments
-# -------------------------
-# Outputs to feeds/articles.xml
-# -------------------------
-# Requires data/articles.json and articles/*.html to exist
-# Requires Perl with JSON::PP
-# Requires UTF-8 support
-# Requires POSIX for strftime
-# Requires Cwd and File::Basename for path handling
-# Requires File::Spec for path handling
-# Requires strict and warnings
-# -------------------------
-# Note: This is a simple script and does not handle all edge cases.
-
 use strict;
 use warnings;
-use JSON::PP;
+
+use File::Find;
 use File::Spec;
-use POSIX          qw(strftime);
-use Cwd            qw(abs_path);
-use File::Basename qw(dirname);
-use Time::Local    qw(timegm);
+use POSIX       qw(strftime);
+use Time::Local qw(timegm);
 
-# -------------------------
-# Logging
-# -------------------------
-my $no_color  = 0;
-my $is_tty    = ( -t STDOUT )             ? 1 : 0;
-my $use_color = ( !$no_color && $is_tty ) ? 1 : 0;
+my $script_dir   = ( File::Spec->splitpath($0) )[1];
+my $root_dir     = File::Spec->catdir( $script_dir, '..' );
+my $feeds_dir    = File::Spec->catdir( $root_dir,   'feeds' );
+my $articles_dir = File::Spec->catdir( $root_dir,   'articles' );
 
-my ( $GREEN, $YELLOW, $RED, $RESET ) = ( "", "", "", "" );
-if ($use_color) {
-    $GREEN  = "\e[32m";
-    $YELLOW = "\e[33m";
-    $RED    = "\e[31m";
-    $RESET  = "\e[0m";
-}
+sub die_tool { die "❌ [ERROR] $_[0]\n"; }
+sub logi     { print "✅ [INFO] $_[0]\n"; }
 
-sub logi { print "${GREEN}✅ [INFO]${RESET} $_[0]\n"; }
-sub logw { print STDERR "${YELLOW}⚠️ [WARN]${RESET} $_[0]\n"; }
-sub loge { print STDERR "${RED}❌ [ERROR]${RESET} $_[0]\n"; }
-
-sub die_tool {
-    my ($msg) = @_;
-    loge($msg);
-    exit 1;
-}
-
-sub read_utf8 {
-    my ($p) = @_;
-    open my $fh, '<:encoding(UTF-8)', $p or return undef;
+sub read_file {
+    my ($path) = @_;
+    open my $fh, '<:encoding(UTF-8)', $path
+      or die_tool("Could not read $path: $!");
     local $/;
-    my $c = <$fh>;
+    my $content = <$fh>;
     close $fh;
-    $c;
+    return $content;
 }
 
-sub write_utf8 {
-    my ( $p, $c ) = @_;
-    open my $fh, '>:encoding(UTF-8)', $p or die_tool "Could not write $p: $!\n";
-    print {$fh} $c;
+sub write_file {
+    my ( $path, $content ) = @_;
+    open my $fh, '>:encoding(UTF-8)', $path
+      or die_tool("Could not write $path: $!");
+    print {$fh} $content;
     close $fh;
 }
 
 sub xml_escape {
-    my ($s) = @_;
-    return '' unless defined $s;
-    $s =~ s/&/&amp;/g;
-    $s =~ s/</&lt;/g;
-    $s =~ s/>/&gt;/g;
-    $s;
-}
-
-sub rfc2822_from_ts_locale {
-    my ( $t, $lang ) = @_;
-    $lang ||= 'en';
-    my @wday_en = qw(Sun Mon Tue Wed Thu Fri Sat);
-    my @mon_en  = qw(Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec);
-    my @wday_es = qw(dom lun mar mié jue vie sáb);
-    my @mon_es  = qw(ene feb mar abr may jun jul ago sep oct nov dic);
-    my ( $sec, $min, $hour, $mday, $mon, $year, $wday ) = gmtime($t);
-    $year += 1900;
-
-    if ( $lang eq 'es' ) {
-        my $wd = $wday_es[$wday] || 'dom';
-        my $mn = $mon_es[$mon]   || 'ene';
-        return sprintf( '%s, %02d %s %d %02d:%02d:%02d GMT',
-            lc($wd), $mday, lc($mn), $year, $hour, $min, $sec );
-    }
-    else {
-        my $wd = $wday_en[$wday] || 'Sun';
-        my $mn = $mon_en[$mon]   || 'Jan';
-        return sprintf( '%s, %02d %s %d %02d:%02d:%02d GMT',
-            $wd, $mday, $mn, $year, $hour, $min, $sec );
-    }
+    my ($text) = @_;
+    return '' unless defined $text;
+    $text =~ s/&/&amp;/g;
+    $text =~ s/</&lt;/g;
+    $text =~ s/>/&gt;/g;
+    return $text;
 }
 
 sub iso_to_epoch {
-    my ($s) = @_;
-    return undef unless defined $s;
-    if ( $s =~
+    my ($iso) = @_;
+    return undef unless defined $iso;
+    if ( $iso =~
 /^(\d{4})-(\d{2})-(\d{2})[T ](\d{2}):(\d{2}):(\d{2})(Z|([+-])(\d{2}):(\d{2}))?$/
       )
     {
-        my ( $Y, $M, $D, $h, $m, $sec, $z, $sign, $oh, $om ) =
+        my ( $Y, $M, $D, $h, $m, $s, undef, $sign, $oh, $om ) =
           ( $1, $2, $3, $4, $5, $6, $7, $8, $9, $10 );
-        $Y   -= 0;
-        $M   -= 1;
-        $D   -= 0;
-        $h   -= 0;
-        $m   -= 0;
-        $sec -= 0;
-        my $epoch = timegm( $sec, $m, $h, $D, $M, $Y );
-
+        my $epoch = timegm( $s, $m, $h, $D, $M - 1, $Y );
         if ( defined $sign && defined $oh ) {
             my $ofs = $oh * 3600 + $om * 60;
             $epoch -= ( $sign eq '+' ) ? $ofs : -$ofs;
         }
         return $epoch;
     }
-    if ( $s =~ /^(\d{4})-(\d{2})-(\d{2})$/ ) {
-        my ( $Y, $M, $D ) = ( $1, $2, $3 );
-        return timegm( 0, 0, 0, $D, $M - 1, $Y );
+    if ( $iso =~ /^(\d{4})-(\d{2})-(\d{2})$/ ) {
+        return timegm( 0, 0, 0, $3, $2 - 1, $1 );
     }
     return undef;
 }
 
-sub first_paragraph {
+sub format_pubdate {
+    my ( $epoch, $lang ) = @_;
+    my @wday_en = qw(Sun Mon Tue Wed Thu Fri Sat);
+    my @mon_en  = qw(Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec);
+    my @wday_es = qw(dom lun mar mié jue vie sáb);
+    my @mon_es  = qw(ene feb mar abr may jun jul ago sep oct nov dic);
+
+    my ( $sec, $min, $hour, $mday, $mon, $year, $wday ) = gmtime($epoch);
+    $year += 1900;
+    if ( $lang eq 'es' ) {
+        return sprintf(
+            '%s, %02d %s %d %02d:%02d:%02d GMT',
+            lc( $wday_es[$wday] ),
+            $mday, lc( $mon_es[$mon] ),
+            $year, $hour, $min, $sec
+        );
+    }
+    return sprintf( '%s, %02d %s %d %02d:%02d:%02d GMT',
+        $wday_en[$wday], $mday, $mon_en[$mon], $year, $hour, $min, $sec );
+}
+
+sub detect_feeds {
+    my %out;
+    my $blog_en = File::Spec->catfile( $feeds_dir, 'blog.xml' );
+    my $blog_es = File::Spec->catfile( $feeds_dir, 'blog-es.xml' );
+    my $art_en  = File::Spec->catfile( $feeds_dir, 'articles.xml' );
+    my $art_es  = File::Spec->catfile( $feeds_dir, 'articles-es.xml' );
+
+    if ( -f $blog_en ) {
+        $out{en} = $blog_en;
+        $out{es} = $blog_es if -f $blog_es;
+        return \%out;
+    }
+    if ( -f $art_en ) {
+        $out{en} = $art_en;
+        $out{es} = $art_es if -f $art_es;
+        return \%out;
+    }
+    die_tool("No supported feed files found in $feeds_dir");
+}
+
+sub extract_metadata {
     my ($path) = @_;
-    return '' unless -e $path;
-    my $html = read_utf8($path) // '';
-    if ( $html =~ /<p>(.*?)<\/p>/s ) {
-        my $p = $1;
-        $p =~ s/<[^>]+>//g;
-        $p =~ s/\s+/ /g;
-        $p =~ s/^\s+|\s+$//g;
-        return $p;
-    }
-    return '';
+    my $html = read_file($path);
+
+    my ($lang) = $html =~ /<html[^>]*\blang\s*=\s*"([^"]+)"/i;
+    $lang = defined $lang && $lang =~ /^es/i ? 'es' : 'en';
+
+    my ($title) = $html =~ /<h1[^>]*>\s*(.*?)\s*<\/h1>/is;
+    $title //= 'Untitled';
+    $title =~ s/<[^>]+>//g;
+    $title =~ s/\s+/ /g;
+    $title =~ s/^\s+|\s+$//g;
+
+    my ($desc) = $html =~ /<p[^>]*>\s*(.*?)\s*<\/p>/is;
+    $desc //= '';
+    $desc =~ s/<[^>]+>//g;
+    $desc =~ s/\s+/ /g;
+    $desc =~ s/^\s+|\s+$//g;
+
+    my ($iso) = $html =~ /<time[^>]*\sdatetime\s*=\s*"([^"]+)"/i;
+    $iso //= ( $html =~ /<time[^>]*\sdatetime\s*=\s*'([^']+)'/i )[0];
+    my $mtime = ( stat($path) )[9] || time;
+    my $epoch = iso_to_epoch($iso);
+    $epoch = $mtime unless defined $epoch;
+
+    my ( undef, undef, $file ) = File::Spec->splitpath($path);
+    $file =~ s/\.html$//i;
+
+    return {
+        slug  => $file,
+        lang  => $lang,
+        title => $title,
+        desc  => $desc,
+        epoch => $epoch,
+    };
 }
 
-my $script_path  = abs_path($0);
-my $root         = File::Spec->catdir( dirname($script_path), '..' );
-my $data_file    = File::Spec->catfile( $root, 'data',  'articles.json' );
-my $feed         = File::Spec->catfile( $root, 'feeds', 'articles.xml' );
-my $articles_dir = File::Spec->catdir( $root, 'articles' );
+sub collect_articles {
+    my ($has_es_feed) = @_;
+    my @rows;
+    find(
+        sub {
+            return unless -f $_;
+            return unless /\.html$/i;
+            return if $_ eq 'index.html';
 
-my $sorted;
+            my $path = $File::Find::name;
+            my $meta = extract_metadata($path);
+            if ( !$has_es_feed ) {
+                $meta->{lang} = 'en';
+            }
+            push @rows, $meta;
+        },
+        $articles_dir
+    );
 
-sub build_items {
-    my @arr;
-    for my $k ( sort keys %$sorted ) {
-        my $e = $sorted->{$k};
-        next unless $e && ref $e eq 'HASH';
-        my $slug = $e->{en} // next;
-        $slug =~ s/\.html$//;
-        my $title   = $e->{title_en} // $slug;
-        my $article = File::Spec->catfile( $articles_dir, "$slug.html" );
-        my $desc    = first_paragraph($article) || '';
-        my $mtime   = undef;
-        if ( exists $e->{pubdate} ) { $mtime = iso_to_epoch( $e->{pubdate} ); }
-        $mtime ||= ( -e $article ) ? ( ( stat($article) )[9] ) : time();
-        my $pub = rfc2822_from_ts_locale( $mtime, 'en' );
-        $title = xml_escape($title);
-        $desc  = xml_escape($desc);
-        my $link = "./../articles/$slug.html";
-        my $guid = $link;
-        my $xml  = "    <item>\n";
-        $xml .= "      <title>$title</title>\n";
-        $xml .= "      <link>$link</link>\n";
-        $xml .= "      <description>$desc</description>\n";
-        $xml .= "      <pubDate>$pub</pubDate>\n";
-        $xml .= "      <guid>$guid</guid>\n";
-        $xml .= "    </item>\n";
-        push @arr, { mtime => $mtime, xml => $xml };
-    }
-    @arr = sort { $b->{mtime} <=> $a->{mtime} } @arr;
-    return join( "\n", map { $_->{xml} } @arr );
-}
-
-sub load_and_sort_articles {
-    my $json_text = read_utf8($data_file) // '{}';
-    my $articles  = eval { JSON::PP->new->utf8->decode($json_text) } || {};
-
-    $sorted = {};
-    for my $k ( sort keys %$articles ) { $sorted->{$k} = $articles->{$k} }
-
-    my $json_out = JSON::PP->new->utf8->canonical->pretty->encode($sorted);
-    write_utf8( $data_file, $json_out );
+    @rows = sort { $b->{epoch} <=> $a->{epoch} } @rows;
+    return \@rows;
 }
 
 sub rebuild_feed_file {
-    my $content = read_utf8($feed)
-      // die_tool "Feed template not found: $feed\n";
-    if ( $content !~ /<channel>/ ) {
-        die_tool "Invalid feed template: $feed\n";
+    my ( $feed_path, $lang, $rows ) = @_;
+    my $xml = read_file($feed_path);
+
+    my ($channel_title) = $xml =~ m{<title>(.*?)</title>}s;
+    $channel_title //= 'RSS Feed';
+    my ($channel_link) = $xml =~ m{<link>(.*?)</link>}s;
+    $channel_link //= '../';
+    my ($channel_desc) = $xml =~ m{<description>(.*?)</description>}s;
+    $channel_desc //= '';
+
+    my $items = '';
+    for my $r (@$rows) {
+        next if $r->{lang} ne $lang;
+        my $pub = format_pubdate( $r->{epoch}, $lang );
+        $items .= join '',
+          "    <item>\n",
+          "      <title>", xml_escape( $r->{title} ), "</title>\n",
+          "      <link>./../articles/$r->{slug}.html</link>\n",
+          "      <description>", xml_escape( $r->{desc} ), "</description>\n",
+          "      <pubDate>$pub</pubDate>\n",
+          "      <guid>./../articles/$r->{slug}.html</guid>\n",
+          "    </item>\n\n";
     }
 
-    my $prefix = $content;
-    my $idx    = index( $content, '</channel>' );
-    if ( $idx == -1 ) {
-        die_tool "Could not find </channel> in template\n";
+    my $last_epoch = time;
+    for my $r (@$rows) {
+        next if $r->{lang} ne $lang;
+        $last_epoch = $r->{epoch};
+        last;
     }
+    my $last_build = format_pubdate( $last_epoch, $lang );
 
-    $prefix = substr( $content, 0, $idx );
-    my $tail = substr( $content, $idx );
-    my $now  = rfc2822_from_ts_locale( time(), 'en' );
-    $prefix =~ s{<pubDate>[^<]*</pubDate>}{<pubDate>$now</pubDate>}m;
-    $prefix =~
-s{<lastBuildDate>[^<]*</lastBuildDate>}{<lastBuildDate>$now</lastBuildDate>}m;
+    my $new_xml = join '',
+      qq(<?xml version="1.0" encoding="UTF-8"?>\n),
+      qq(<rss version="2.0">\n),
+      qq(  <channel>\n),
+      qq(    <title>$channel_title</title>\n),
+      qq(    <link>$channel_link</link>\n),
+      qq(    <description>$channel_desc</description>\n),
+      qq(    <lastBuildDate>$last_build</lastBuildDate>\n\n),
+      $items,
+      qq(  </channel>\n),
+      qq(</rss>\n);
 
-    $prefix =~ s{<item>.*?</item>\s*}{}gs;
-    $prefix =~ s/\s+$//s;
-    $prefix .= "\n\n";
-
-    my $items = build_items();
-    my $new   = $prefix . $items . "\n" . $tail;
-    $new =~ s/\n{3,}/\n\n/g;
-    write_utf8( $feed, $new );
-    logi("Wrote $feed");
-}
-
-sub run_rebuild {
-    load_and_sort_articles();
-    rebuild_feed_file();
+    write_file( $feed_path, $new_xml );
+    logi("Rebuilt feed: $feed_path");
 }
 
 sub main {
-    run_rebuild();
+    my $feeds       = detect_feeds();
+    my $has_es_feed = exists $feeds->{es} ? 1 : 0;
+    my $rows        = collect_articles($has_es_feed);
+
+    rebuild_feed_file( $feeds->{en}, 'en', $rows );
+    rebuild_feed_file( $feeds->{es}, 'es', $rows ) if $has_es_feed;
 }
 
 main();
-exit 0;
+
